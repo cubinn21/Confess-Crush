17f9db6e1f9e9945625832b75e6e0df8c11e9b17
cubinn21 <104311573+cubinn21@users.noreply.github.com>
2022-04-24 21:37:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tỏ tình Crush Cuteee <3",
-    introTitle: 'Trần Nguyễn Trúc Linh',
+    introTitle: 'Hoàng Bảo Thiêm',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -8,7 +8,7 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'Dzaaa <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu mình <3',
     btnReply: 'Gửi cho cậu <3',
